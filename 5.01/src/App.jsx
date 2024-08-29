3e5c1ca3d7488f3c8e053821237d700c03372152
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-08-30 00:52:34 +0300

    feat: practice

---
+++ 5.01/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <h1 className="hello">Hello</h1>
+    );
+};
+
+export default App;
---
+++ 5.01/src/test.js
@@ -0,0 +1,4 @@
+const TEST1 = 5;
+const TEST2 = 6;
+
+export  { TEST1, TEST2 };
