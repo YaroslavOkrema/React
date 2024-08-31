dbcd872e737fbd8dfef657f5fe362cb42d0356ef
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-08-31 14:54:49 +0300

    feat: key

--- 5.01/src/App.jsx
+++ 5.01/src/App.jsx
@@ -1,68 +1,19 @@
 import React from "react";
 
+const USERS = [
+    { name: 'Alex'},
+    { name: 'Ivan' },
+    { name: 'Roma' },
+];
+
 const App = () => {
-    const array = [
-        {hello: 'hello'},
-        {world: 'world'}
-    ];
-
-    const arrayReactElements = [];
-
-    for (let i = 0; i < array.length; i++) {
-        const obj = array[i];
-
-        arrayReactElements.push(
-            <div>{obj.hello}</div>
-        )
-    }
-
-    console.log(array);
-    console.log(arrayReactElements);
-
     return (
         <div>
-            {array.map((obj) => {
-                return (
-                  <p>{obj.world}</p>
-                );
-            })}
+            {USERS.map((user, index) => (
+                <p key={user.name}>{user.name}</p>
+            ))}
         </div>
     );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   /* const name = 'Alex';
-    const age = 15;
-
-    let attr = 'hello';
-
-    if (age === 18) {
-        attr = 'good';
-    } else {
-        attr = 'bad';
-    }
-
-    function test() {
-        if (age === 18) {
-            return (
-              <div>1</div>
-            );
-        } else {
-            return (
-                <div>2</div>
-            );
-        }*/
 }
 
 export default App;
