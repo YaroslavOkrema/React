a76ac9a488214f56808c048dcb763b941f6b9f19
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-08-31 14:31:25 +0300

    feat: practice jsx

--- 5.01/src/App.jsx
+++ 5.01/src/App.jsx
@@ -1,9 +1,68 @@
 import React from "react";
 
 const App = () => {
+    const array = [
+        {hello: 'hello'},
+        {world: 'world'}
+    ];
+
+    const arrayReactElements = [];
+
+    for (let i = 0; i < array.length; i++) {
+        const obj = array[i];
+
+        arrayReactElements.push(
+            <div>{obj.hello}</div>
+        )
+    }
+
+    console.log(array);
+    console.log(arrayReactElements);
+
     return (
-        <h1 className="hello">Hello</h1>
+        <div>
+            {array.map((obj) => {
+                return (
+                  <p>{obj.world}</p>
+                );
+            })}
+        </div>
     );
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   /* const name = 'Alex';
+    const age = 15;
+
+    let attr = 'hello';
+
+    if (age === 18) {
+        attr = 'good';
+    } else {
+        attr = 'bad';
+    }
+
+    function test() {
+        if (age === 18) {
+            return (
+              <div>1</div>
+            );
+        } else {
+            return (
+                <div>2</div>
+            );
+        }*/
+}
 
 export default App;
