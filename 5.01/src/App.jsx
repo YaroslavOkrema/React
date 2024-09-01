34b7d82472a857feb772183ca3b0fd9bcabe4803
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-09-01 08:45:11 +0300

    feat: events

--- 5.01/src/App.jsx
+++ 5.01/src/App.jsx
@@ -1,56 +1,19 @@
 import React from "react";
 import UserName from "./UserName";
 
-let USERS = [
-    { id: 1, name: 'Alex', surname: 'Fox', age: 20},
-    { id: 2, name: 'Ivan', surname: 'Crab', age: 21},
-    { id: 3, name: 'Roma', surname: 'Chopa', age: 23},
-    { id: 4, name: 'Vlad', surname: 'Acrabat', age: 24},
-];
-
 const App = () => {
-    const changeName = (id, newName) => {
-        USERS.map((user) => {
-            if (user.id === id) {
-                return {
-                    id: user.id,
-                    name: newName,
-                    surname: user.surname,
-                    age: user.age,
-                };
-            }
-
-            return user;
-        });
-    };
-
-    const data = [];
-        {USERS.map((user, index) => (
-            data.push(
-                <div>
-                    <UserName
-                        userName={user.name}
-                        userSurname={user.surname}
-                        userAge={user.age}
-                        changeName={changeName}
-                        /*test="hello"
-                        test2={1321}
-                        test3={true}
-                        test4={{hello: 'world'}}
-                        test5={{test: '123'}}
-                        test6={() => {}}
-                        test7={'qwe'}
-                        test8*/
-                    />
-                </div>
-            )
-        ));
-        }
+    const onClickHandler = (event) => {
+        console.log(event);
+    }
     return (
-        <div>
-            {data}
+        <div onClick={onClickHandler}>
+            <input type="text" onChange={(event) => {
+                console.log('on change', event.target.value);
+            }}/>
+            <button>Click</button>
         </div>
     );
 }
+
 
 export default App;
